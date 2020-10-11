b0e8bebbe5120f0dd38e00e193539ba37277a7b1
shleewhite <leewhite128@gmail.com>
2020-10-10 22:50:03 -0700

    slowly adding firebase stuff back in

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function PageNotFound() {
+  return (
+    <Layout title="404 | A11ies.info">
+      <div>
+        <h1>404</h1>
+        <p>Page not found. Sorry!</p>
+      </div>
+    </Layout>
+  )
+}
