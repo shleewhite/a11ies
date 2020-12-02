1f185701c11e6cd382f9178b46bfff9b0e7c725a
shleewhite <leewhite128@gmail.com>
2020-12-02 15:54:51 -0800

    finishing up v1

--- pages/admin.js
+++ pages/admin.js
@@ -6,19 +6,24 @@ import {
   DropdownMenu,
   DropdownItem,
 } from "reactstrap";
-// import Link from "next/link";
+import Link from "next/link";
 
 import Layout from "../components/Layouts/Layout";
 
 import PageNotFound from "./404";
 
 import { UserContext } from "../lib/user_context";
-import { getAdminData, ADMIN_TABLE_HEADERS } from "../lib/admin";
+import { getAdminData, ADMIN_TABLES } from "../lib/admin";
 
 export default function Admin() {
   const context = useContext(UserContext);
   const [adminData, setAdminData] = useState([]);
   const accessLevel = context.isLoggedIn ? context.user.accessLevel : -1;
+
+  const [dropdownOpen, setDropdownOpen] = useState("");
+  const toggleDropdown = (id) => {
+    setDropdownOpen((prevState) => (prevState === id ? "" : id));
+  };
 
   useEffect(() => {
     async function getData() {
@@ -28,58 +33,68 @@ export default function Admin() {
     if (context.isLoggedIn && adminData.length === 0) getData();
   }, [adminData, setAdminData, accessLevel]);
 
-  const renderTable = (id, data) => {
-    const columns = Object.values(ADMIN_TABLE_HEADERS[id]);
+  const renderTable = (collection, data) => {
+    const columns = Object.values(ADMIN_TABLES[collection]["column-headers"]);
+    const actions = Object.values(ADMIN_TABLES[collection].actions);
     return (
       <Table bordered>
         <thead>
           <tr>
-            {columns.map((colData) => (
-              <th scope="col">{colData.name}</th>
+            {columns.map((colData, i) => (
+              <th scope="col" key={`${colData.id}-${i}`}>
+                {colData.name}
+              </th>
             ))}
             <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((row) => (
-            <tr>
-              {columns.map((colData) => {
-                const val = row[colData.id];
-                if (typeof val === "object") {
-                  return (
-                    <td>
-                      <a href={val.link}>{val.name}</a>
-                    </td>
+          {data.map((row, i) => (
+            <tr key={`${collection}-row-${i}`}>
+              {columns.map((colData, j) => {
+                let val = row[colData.id];
+
+                if (val instanceof Array) val = val.toString();
+                else if (
+                  collection === "trancripts" &&
+                  typeof val === "object"
+                ) {
+                  val = (
+                    <Link href={val.link}>
+                      <a>{val.name}</a>
+                    </Link>
                   );
-                } else if (typeof val === "boolean") {
-                  return <td>{val ? "true" : "false"}</td>;
-                } else return <td>{val}</td>;
+                } else if (typeof val === "object")
+                  val = <a href={val.link}>{val.name}</a>;
+                else if (typeof val === "boolean") val = val ? "true" : "false";
+
+                return <td key={`${collection}-row-${i}-cell-${j}`}>{val}</td>;
               })}
               <td>
-                <Dropdown>
-                  <DropdownToggle caret />
+                <Dropdown
+                  isOpen={dropdownOpen === `${collection}-row-${i}-action`}
+                  toggle={() => {
+                    toggleDropdown(`${collection}-row-${i}-action`);
+                  }}
+                  id={`${collection}-row-${i}-action`}
+                >
+                  <DropdownToggle>Actions</DropdownToggle>
                   <DropdownMenu>
-                    <DropdownItem>Test</DropdownItem>
+                    {actions.map((action, j) => (
+                      <DropdownItem
+                        key={`${collection}-row-${i}-action-${j}`}
+                        onClick={() => {
+                          action.function(row.id, collection);
+                        }}
+                      >
+                        {action.name}
+                      </DropdownItem>
+                    ))}
                   </DropdownMenu>
                 </Dropdown>
               </td>
             </tr>
           ))}
-          {/* {data.map((row) => (
-            <tr>
-              {Object.values(row).map((val) => {
-                if (typeof val === "object") {
-                  return (
-                    <td>
-                      <a href={val.link}>{val.name}</a>
-                    </td>
-                  );
-                } else if (typeof val === "boolean") {
-                  return <td>{val ? "true" : "false"}</td>;
-                } else return <td>{val}</td>;
-              })}
-            </tr>
-          ))} */}
         </tbody>
       </Table>
     );
@@ -88,10 +103,10 @@ export default function Admin() {
   const renderAdminContent = () => (
     <>
       {Object.entries(adminData).map(([id, section]) => (
-        <>
+        <div key={id}>
           <h2>{section.name}</h2>
           {renderTable(id, section.data)}
-        </>
+        </div>
       ))}
     </>
   );
