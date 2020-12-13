be758356f60b50526579c99c5fe7b805e71269f6
shleewhite <leewhite128@gmail.com>
2020-12-13 12:08:42 -0800

    adding in google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,45 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const initialProps = await NextDocument.getInitialProps(ctx);
-    return { ...initialProps };
+    const isProduction = process.env.NODE_ENV === "production";
+
+    return { ...initialProps, isProduction };
   }
 
   render() {
+    const { isProduction } = this.props;
+
     return (
       <Html lang="en" color-mode="light">
-        <Head />
+        <Head>
+          {isProduction && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
