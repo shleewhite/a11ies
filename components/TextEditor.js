b6527ec38eae229af38d66824d7ad9c1dad19eee
Cordelia <cordeliadillon@users.noreply.github.com>
2020-12-21 19:26:03 -0800

    Update /contribute form styling, add descriptions to inputs, add basic short URL validation (#11)
    * Update input field styling and optional input descriptions
    * Update CSS variables to include a medium shade background
    * Update /contribute form layout to be responsive
    * Add basic short URL validation to /contribute (still more to do here!)
    * Fix issue with auth modal not displaying

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -17,7 +17,7 @@ const CONFIG = {
       "|",
       "link",
     ],
-  },
+  }
 };
 
 class TextEditor extends React.Component {
@@ -51,6 +51,9 @@ class TextEditor extends React.Component {
   render() {
     return (
       <>
+        <small id={this.state.keyboardInfoId}>
+          Press Alt + F10 to navigate to text editor toolbar.
+        </small>
         {this.CKEditor && (
           <this.CKEditor
             editor={this.ClassicEditor}
@@ -76,18 +79,25 @@ class TextEditor extends React.Component {
             }}
           />
         )}
-        <small
-          className="f6 black-60 db mt1 mb3"
-          id={this.state.keyboardInfoId}
-        >
-          Press Alt + F10 to navigate to text editor toolbar.
-        </small>
         <textarea
           name={this.props.name}
           readOnly
           style={{ display: "none" }}
           value={this.state.markdown}
         />
+        <style jsx>
+          {`
+            .ck-editor__editable_inline {
+              min-height: 20em;
+            }
+
+            small {
+              display: block;
+              font-size: var(--text-s);
+              padding-bottom: var(--space-s);
+            }
+          `}
+        </style>
       </>
     );
   }
--- pages/contribute/index.js
+++ pages/contribute/index.js
@@ -8,9 +8,30 @@ import TextEditor from "../../components/TextEditor";
 import FormAuth from "../../components/FormAuth";
 import FormSuccess from "../../components/FormSuccess";
 
+import { RESERVED_PATHS, BREAKPOINTS } from "../../lib/constants";
 import { createTranscript } from "../../lib/transcripts";
 // TODO: Url validation: import { URL_REGEX } from '../constants';
 // TODO: Url validation: throw an error if the short url already exists
+
+const errors = {
+  required: `Whoops! Don't forget this field is required.`,
+  unavailableURL: `Alas, this short URL isn't available. Please choose 
+    another one or leave blank to have one auto-generated.`,
+  invalidURL: `Whoops! We can only accept short URLs with letters, numbers,
+    hyphens, and/or underscores. Please correct stray characters.`
+};
+
+function hasReservedPathError(url) {
+  let lcURL = url.toLowerCase();
+  for (let i = 0; i < RESERVED_PATHS.length; i++) {
+    if (lcURL === RESERVED_PATHS[i].toLowerCase()) return true;
+  }
+  return false;
+}
+
+function hasInvalidCharacters(url) {
+  return !(/^[\w\d_-]*$/.test(url));
+}
 
 export default function Create() {
   const [isPublished, setIsPublished] = useState(false);
@@ -19,6 +40,25 @@ export default function Create() {
     isLoggedIn: false,
     uid: "",
   });
+
+  // TODO, streamline error messages w/ arrays?
+  const [transcriptError, setTranscriptError] = useState(null);
+  const [urlError, setURLError] = useState(null);
+
+  const validateClientsideURL = (url) => {
+    if (hasReservedPathError(url)) {
+      setURLError(errors.unavailableURL);
+      return false;
+    }
+
+    if (hasInvalidCharacters(url)) {
+      setURLError(errors.invalidURL);
+      return false;
+    }
+
+    setURLError(null);
+    return true;
+  }
 
   const submitTranscript = (event) => {
     event.preventDefault();
@@ -32,26 +72,38 @@ export default function Create() {
               .replace("#", "")
               .split(/\s*,*\s+#*/);
 
-      setURL(data.get("url").length > 0 ? nolookalikes(6) : data.get("url"));
+      let tempURL = data.get("url").length > 0 ? data.get("url") : nolookalikes(6);
 
-      createTranscript(
-        url,
-        {
-          link: data.get("link"),
-          name: data.get("name"),
-          creatorName: data.get("creatorName"),
-          creatorLink: data.get("creatorLink"),
-          transcript: data.get("transcript"),
-          searchable: data.get("searchable") !== null,
-          publishDate: Date.now(),
-          hashtags,
-          uid,
-        },
-        () => {
-          setIsPublished(true);
-        }
-      );
+      if (validateClientsideURL(tempURL)) {
+        setURL(tempURL);
+        createTranscript(
+          tempURL,
+          {
+            link: data.get("link"),
+            name: data.get("name"),
+            creatorName: data.get("creatorName"),
+            creatorLink: data.get("creatorLink"),
+            transcript: data.get("transcript"),
+            searchable: data.get("searchable") !== null,
+            publishDate: Date.now(),
+            hashtags,
+            uid,
+          },
+          /* success callback */
+          () => {
+            setIsPublished(true);
+          },
+          /* failure callback */
+          () => {
+            setURLError(errors.unavailableURL);
+          }
+        );
+      }
     }
+  };
+
+  const handleErrors = () => {
+
   };
 
   const resetForm = () => {
@@ -74,7 +126,7 @@ export default function Create() {
           </FormSuccess>
         ) : (
           <form action="" className="" onSubmit={submitTranscript} id="create">
-            <Input label="Document Name (required)" required id="name" />
+            <Input label="Document title (required)" required id="name" />
 
             <Input
               label="Link to original document (required)"
@@ -91,29 +143,70 @@ export default function Create() {
               id="creatorLink"
             />
 
-            <span className="f6 db mb2">
-              <span className="b">Transcript</span> (required)
-            </span>
-            <TextEditor
-              name="transcript"
-              label="Transcript (required), Rich Text Editor"
-              id="transcript"
-            />
+            <div className="full-width-input">
+              <span>
+                <span className="b">Transcript</span> (required)
+              </span>
+              <TextEditor
+                name="transcript"
+                label="Transcript (required), Rich Text Editor"
+                id="transcript"
+              />
+            </div>
 
-            <Input label="Relevant hashtags" id="hashtags" />
+            <Input label="Relevant hashtags" id="hashtags" type="textarea"
+              description="Use a comma-separated list and capitalize the first letter
+                of each word, e.g. #ThisIsAHashtag, #ThisIsAnother" />
+
+            <div></div>
+
+            <div className="full-width-input">
+              <Input
+                label="Make transcript searchable"
+                description="Transcript can appear in a11ies.info browse views and search results"
+                type="checkbox"
+                id="searchable"
+              />
+            </div>
 
             <Input
-              label="Transcript can appear in a11ies.info search results"
-              type="checkbox"
-              id="searchable"
+              label="Custom short URL"
+              prefix="a11ies.info/"
+              description="If you don't provide one, we'll generate one for you!"
+              id="url" 
+              variant="short" 
+              error={urlError}
             />
 
-            <Input label="Custom URL for transcript" id="url" />
+            <div></div>
 
             <input type="submit" value="Publish Transcript" name="submit" />
           </form>
         )}
       </SecondaryNavLayout>
+      <style jsx>
+        {`
+          form {
+            display: grid;
+            grid-template-columns: 1fr;
+            grid-gap: var(--space-m);
+            gap: var(--space-m);
+          }
+
+          @media ${BREAKPOINTS.MEDIUM_LARGE} {
+            form {
+              width: 80%;
+              grid-template-columns: 2fr 3fr;
+              grid-gap: var(--space-l) var(--space-m);
+              gap: var(--space-l) var(--space-m);
+            }
+
+            .full-width-input {
+              grid-column: 1 / span 2;
+            }
+          } 
+        `}
+      </style>
     </FormAuth>
   );
 }
